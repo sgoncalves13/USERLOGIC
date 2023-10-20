b3e912345b71179dc6c9d801a6a1bd59da1e1bd2
sgoncalves13 <s.goncalves@uniandes.edu.co>
2023-10-19 22:03:20 -0500

    aa

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class HistoriaClinica(models.Model):
     id_hc = models.AutoField(primary_key=True)
-    diagnosticos = models.CharField(max_length=None)
-    tratamientos = models.CharField(max_length=None)
-    notas = models.CharField(max_length=None)
+    diagnosticos = models.CharField(max_length=500)
+    tratamientos = models.CharField(max_length=500)
+    notas = models.CharField(max_length=500)
 
 class Usuario(models.Model):
     documento = models.CharField(max_length=15, primary_key=True)
@@ -22,7 +22,7 @@ class Adenda(models.Model):
     id_adenda = models.AutoField(primary_key=True)
     fecha = models.CharField(max_length=40)
     tipo = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=None)
+    descripcion = models.CharField(max_length=500)
     historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE, related_name='adendas')
 
 
