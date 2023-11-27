2e2187d54386ce343814457440d49f3f7550c1d4
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 11:59:28 -0500

    cambios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from .views import usuarioAPI, historiaClinicaAPI, agregarAdendaAPI
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("usuario/", usuarioAPI.as_view()),
     path("historia_clinica/", historiaClinicaAPI.as_view()),
     path("agregar_adenda/", agregarAdendaAPI.as_view())
