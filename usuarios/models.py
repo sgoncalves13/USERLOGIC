40ce723c1585a9f5e3b572caa5fa04a7b745a21a
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 11:44:49 -0500

    cambios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,28 +1,25 @@
 from django.db import models
+import uuid
 
 class HistoriaClinica(models.Model):
-    id_hc = models.AutoField(primary_key=True)
     diagnosticos = models.CharField(max_length=500)
     tratamientos = models.CharField(max_length=500)
     notas = models.CharField(max_length=500)
 
-class Usuario(models.Model):
-    documento = models.CharField(max_length=15, primary_key=True)
-    clave = models.CharField(max_length=30, default='a')
-    tipo = models.CharField(max_length=30, default='a')
-    nombre = models.CharField(max_length=30, default='a')
-    edad = models.CharField(max_length=30, default='a')
-    telefono = models.CharField(max_length=30, default='a')
-    sexo = models.CharField(max_length=30, default='a')
-    foto = models.CharField(max_length=255, default='a')
-    historia_clinica = models.OneToOneField(HistoriaClinica, on_delete=models.CASCADE, related_name='usuario', null=True, blank=True)
-    medico = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='pacientes')
-
 class Adenda(models.Model):
-    id_adenda = models.AutoField(primary_key=True)
     fecha = models.CharField(max_length=40)
     tipo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=500)
     historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE, related_name='adendas')
 
-
+class Usuario(models.Model):
+    documento = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    clave = models.CharField(max_length=30, default='123')
+    tipo = models.CharField(max_length=30, default='default')
+    foto = models.CharField(max_length=255, default='default')
+    nombre = models.CharField(max_length=30, default='default')
+    edad = models.CharField(max_length=30, default='default')
+    telefono = models.CharField(max_length=30, default='default')
+    sexo = models.CharField(max_length=30, default='default')
+    historia_clinica = models.OneToOneField(HistoriaClinica, on_delete=models.CASCADE, related_name='usuario', null=True, blank=True)
+    medico = models.OneToOneField('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='paciente', limit_choices_to={'tipo': 'paciente'})
