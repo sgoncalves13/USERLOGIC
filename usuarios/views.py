10bbfb6e81977250d265b4ea3f0497a1349d09ff
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-27 12:15:45 -0500

    cambios

--- usuarios/views.py
+++ usuarios/views.py
@@ -47,10 +47,10 @@ def agregar_usuario(documento, clave, tipo, nombre, edad, telefono, sexo, foto):
     
 def asignar_medico_a_paciente(documento_profesional, documento_paciente):
     try:
-        profesional = Usuario.objects.get(documento=documento_profesional, tipo='medico')
+        profesional = Usuario.objects.get(documento=documento_profesional, tipo='profesionalSalud')
         paciente = Usuario.objects.get(documento=documento_paciente, tipo='paciente')
         if not paciente.medico:
-            paciente.medico = profesional
+            paciente.medico = documento_profesional
             paciente.save()
             
         return paciente
