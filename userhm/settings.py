74fab76b5eab9d9641645cc992c8ccdc8a0f4043
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-12-04 20:13:05 -0500

    cambios

--- userhm/settings.py
+++ userhm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'svm_)tpa-o^gkn@81sel&lapq2jc7^^-n9c+4y&f9rymz$kum_'
-SIMETRIC_KEY = b'SebastianRamirezLeMeteMuyDuro'
+SIMETRIC_KEY = 'SebastianRamirezLeMeteMuyDuro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
